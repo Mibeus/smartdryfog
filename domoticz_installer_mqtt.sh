1aa7eb4fdcef9582e9218f406c527c5db7816c04
Mibeus <55436279+Mibeus@users.noreply.github.com>
2025-07-21 22:06:02 +0200

    Add files via upload

--- domoticz_installer_mqtt.sh
+++ domoticz_installer_mqtt.sh
@@ -4,11 +4,16 @@
 # Zastavime pri chybe
 set -e
 
-
 # Vytvorenie uzivatela pre MQTT
 echo "Vytvoram MQTT uzivatela..."
-sudo mosquitto_passwd -c /etc/mosquitto/passfile dryfogmqtt
+MQTT_PASSWORD="Vgy5Js9TYcuh"
+echo "$MQTT_PASSWORD" | sudo mosquitto_passwd -c /etc/mosquitto/passfile dryfogmqtt
+echo "MQTT uzivatel vytvoreny s heslom: $MQTT_PASSWORD"
+echo "Heslo ulozene v /home/RTTSK/mqtt_password.txt"
+echo "Uzivatel: dryfogmqtt" > /home/RTTSK/mqtt_password.txt
+echo "Heslo: $MQTT_PASSWORD" >> /home/RTTSK/mqtt_password.txt
 mosquitto -h | grep version
+
 
 # Uprava konfiguracie Mosquitto
 echo "Upravenie konfiguracie Mosquitto..."
